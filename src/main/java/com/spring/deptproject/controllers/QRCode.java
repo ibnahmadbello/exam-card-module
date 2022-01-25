bf0a7bf632e60c3ac43a730a0a49d4ec80135148
ibnahmadbello <justforlearningcode@gmail.com>
2022-01-25 17:29:03 +0100

    Refactored some working

--- src/main/java/com/spring/deptproject/controllers/QRCode.java
+++ src/main/java/com/spring/deptproject/controllers/QRCode.java
@@ -7,11 +7,9 @@ import java.util.Map;
 import java.util.Map.Entry;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
